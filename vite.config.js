5785afef2a99b1103a5077ab94d487d7ce2a6795
secondhanddev <>
2025-12-04 18:21:14 +1100

    Fix for `npm run build` warnings

--- vite.config.js
+++ vite.config.js
@@ -7,30 +7,38 @@ const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    root: path.resolve(__dirname, 'src'),
-    server: {
-        port: 8000,
-        hot: true,
-    },
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
-            'bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-            'hammerjs': path.resolve(__dirname, 'node_modules/hammerjs'),
-            'showdown': path.resolve(__dirname, 'node_modules/showdown'),
-            '@capacitor/core': path.resolve(__dirname, 'node_modules/@capacitor/core'),
-        }
-    },
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                    }
-                }
-            }
-        }
-    }
+	root: path.resolve(__dirname, 'src'),
+	server: {
+		port: 8000,
+		hot: true,
+	},
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+			'bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+			'hammerjs': path.resolve(__dirname, 'node_modules/hammerjs'),
+			'showdown': path.resolve(__dirname, 'node_modules/showdown'),
+			'@capacitor/core': path.resolve(__dirname, 'node_modules/@capacitor/core'),
+		}
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				api: 'modern-compiler',
+				silenceDeprecations: ['legacy-js-api', 'import', 'global-builtin', 'color-functions', 'abs-percent']
+			}
+		}
+	},
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes('node_modules')) {
+						return id.toString().split('node_modules/')[1].split('/')[0].toString();
+					}
+				}
+			}
+		}
+	}
 })
