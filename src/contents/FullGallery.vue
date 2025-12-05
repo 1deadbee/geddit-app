de240a1670270be687e0528cb37ee2787f26e23b
secondhanddev <>
2025-12-06 09:50:30 +1100

    Image download button for multi-image posts

--- src/contents/FullGallery.vue
+++ src/contents/FullGallery.vue
@@ -1,11 +1,16 @@
 <template>
     <div class="wrapper">
-        <div v-for="image in images">
-            <img :src="image.src" @click.prevent="emit('open_post')">
+        <div v-for="(image, index) in images" :key="index">
+            <img :src="image.src" @click.prevent="toggle_controls">
             <div class="position-absolute top-0 end-0 m-2">
                 <div class="d-flex position-relative">
                     <div class="position-absolute background cover-all opacity-75 rounded"></div>
                     <small class="gallery-indicator position-relative text-4 px-1" :length="images.length"></small>
+                </div>
+            </div>
+            <div class="position-absolute bottom-0 end-0 z-1 m-3" v-show="controls_visible">
+                <div class="md-fab md-foreground el-3" @click.passive="download(image.src, index)">
+                    <span class="material-icons text-4">download</span>
                 </div>
             </div>
         </div>
@@ -14,6 +19,8 @@
 
 <script setup>
 import { onMounted, ref } from 'vue';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Toast } from '@capacitor/toast';
 
 const emit = defineEmits(['open_post']);
 const props = defineProps({
@@ -24,6 +31,31 @@ const props = defineProps({
 })
 
 const images = ref([]);
+const controls_visible = ref(false);
+
+async function toggle_controls() {
+    controls_visible.value = !controls_visible.value;
+}
+
+async function download(imageUrl, index) {
+    let filename = `${Date.now()}_${props.data.id}_${index}`;
+
+    let extension = new URL(imageUrl).pathname.split('.').pop();
+
+    Filesystem.downloadFile({
+        url: imageUrl,
+        directory: Directory.Documents,
+        path: `/geddit/${filename}.${extension}`
+    }).then((res) => {
+        console.log(res);
+        Toast.show({
+            text: 'Image saved to gallery',
+            duration: 'short'
+        });
+    }).catch((error) => {
+        console.error(error);
+    })
+}
 
 async function get_sources() {
     if (!props.data.gallery_data) return;
